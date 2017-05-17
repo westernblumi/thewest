79e287f078d9dcf767f9536e9d370f07734b1e8a
westernblumi <19blumi95@web.de>
2017-05-17 21:38:57 +0200

    Update skriptUpdater.js

--- skriptUpdater.js
+++ skriptUpdater.js
@@ -3,6 +3,6 @@ var scriptUpdate={
 	TWFBTNew:'Small bug fix',
 	announcementBlock: '1.02',
 	announcementBlockNew: 'Support for other language servers',
-	WTK:'1.01',
-	WTKNew:'Small bug fixes',
+	WTK:'1.03',
+	WTKNew:'New Language: Polish\nNew Feature: Opens the mobil traider, when new items are available\nNew Feature: Enables ignoring other players in adventure',
 };
