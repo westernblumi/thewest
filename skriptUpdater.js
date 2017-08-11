44cff8cd438c010b559975b551097edc4592825f
westernblumi <19blumi95@web.de>
2017-08-11 18:28:29 +0200

    Update skriptUpdater.js

--- skriptUpdater.js
+++ skriptUpdater.js
@@ -1,6 +1,6 @@
 var scriptUpdate={
-	TWFBT:'0.52',
-	TWFBTNew:'Small bug fix',
+	TWFBT:'1.10',
+	TWFBTNew:'New language: Italiano by Ruslan Jackson\nNew feature:  Class specific icons in fortbattle line-up view\nNew feature: Fortbattle statistics: team, player, weapons, order of movement and a ranking with Innogames brand-new formula',
 	announcementBlock: '1.02',
 	announcementBlockNew: 'Support for other language servers',
 	WTK: '1.04',
