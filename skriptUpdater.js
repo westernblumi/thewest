3337fd847003410e2e4691c5d414ab30fd4b5c30
westernblumi <19blumi95@web.de>
2017-08-18 14:25:35 +0200

    Update skriptUpdater.js

--- skriptUpdater.js
+++ skriptUpdater.js
@@ -1,6 +1,6 @@
 var scriptUpdate={
-	TWFBT:'1.10',
-	TWFBTNew:'New language: Italiano by Ruslan Jackson\nNew feature:  Class specific icons in fortbattle line-up view\nNew feature: Fortbattle statistics: team, player, weapons, order of movement and a ranking with Innogames brand-new formula',
+	TWFBT:'1.11',
+	TWFBTNew:'New buff statistics and full compatibility with "The West - Toolkit"',
 	announcementBlock: '1.02',
 	announcementBlockNew: 'Support for other language servers',
 	WTK: '1.04',
