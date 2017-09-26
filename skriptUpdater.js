ff2040e2fcbf9450e43eadfb0122645ac072d188
westernblumi <19blumi95@web.de>
2017-09-26 21:56:58 +0200

    Update skriptUpdater.js

--- skriptUpdater.js
+++ skriptUpdater.js
@@ -1,8 +1,8 @@
 var scriptUpdate={
-	TWFBT:'1.12',
-	TWFBTNew:'New statistic: time of death',
+	TWFBT:'1.13',
+	TWFBTNew:'New round statistic + statistic export',
 	announcementBlock: '1.02',
 	announcementBlockNew: 'Support for other language servers',
-	WTK: '1.05',
-	WTKNew: 'New Features: Church level + Item Finder',
+	WTK: '1.06',
+	WTKNew: 'New Features: WindowPin + FormattedRankings + improved build windows',
 };
