5e3b61157e7905b9c1d3173ff95b63b9dd2fd9d0
westernblumi <19blumi95@web.de>
2017-10-03 11:15:42 +0200

    Update skriptUpdater.js

--- skriptUpdater.js
+++ skriptUpdater.js
@@ -1,8 +1,8 @@
 var scriptUpdate={
-	TWFBT:'1.13',
-	TWFBTNew:'New round statistic + statistic export',
+	TWFBT:'1.14',
+	TWFBTNew:'Bug fixes + statistic for the adventurer bonus',
 	announcementBlock: '1.02',
 	announcementBlockNew: 'Support for other language servers',
-	WTK: '1.06',
-	WTKNew: 'New Features: WindowPin + FormattedRankings + improved build windows',
+	WTK: '1.07',
+	WTKNew: 'Bug fixes',
 };
